883271596699fb2f7b1353f1c2c3de7d46da7121
william-coleman <williamcoleman@WilliamColemanMBP.local>
2016-09-22 08:58:37 -0400

    Changes and wednesday homework

--- day2/weather.js
+++ day2/weather.js
@@ -1,10 +1,22 @@
-$(document).ready()(function(){ 
+$(document).ready()(function () {
 
-  ("#add").click(function(ev) {
-       ("ul").append('<li>Item 2<button class="remove">Remove</button></li>');
-   });
+    //   ("#add").click(function(ev) {
+    //        ("ul").append('<li>Item 2<button class="remove">Remove</button></li>');
+    //    });
 
-   ("ul").click(".remove", function(ev) {
-       (ev.target).parent().fadeOut(); //.remove();
-   });
+    //    ("ul").click(".remove", function(ev) {
+    //        (ev.target).parent().fadeOut(); //.remove();
+    //    });
+    var url = "http://rest.learncode.academy/api/learncode/friends/57e2c0f71ebec60100061868"
+    $.ajax(url).done(function (data) {
+        $("#friend").append(data.name);
+    });
+
+
+
+
+
+
+
+
 });
