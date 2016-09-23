a25ad9a1109557ede457d3aa2658caff5b667da5
william-coleman <williamcoleman@WilliamColemanMBP.local>
2016-09-23 09:48:14 -0400

    Making a project folder and posting updates

---
+++ Day4/practice.js
@@ -0,0 +1,23 @@
+var paladins = {
+    Orinate: {
+        health: 98,
+        mana: 150,
+        weapon: [
+            { name: "Wand", damage: 37}
+        ]
+    }
+    Jofrey: {
+        health: 210,
+        mana: 50,
+        weapon: [
+            {name: "Axe", damage: 165}
+        ]
+    }
+    Strider: {
+        health: 63,
+        mana: 100,
+        weapon: [
+            {name: "Crossbow", damage: 74}
+        ]
+    }
+}
